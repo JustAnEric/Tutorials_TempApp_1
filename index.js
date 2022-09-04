adc8e55ed7e2741fb11f4d23f1bb9cd967a7217d
WWEMGamer2 <73326807+WWEMGamer2@users.noreply.github.com>
2022-09-04 10:56:22 +1000

    added location functions

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 var prompt = document.querySelector('promt');
 var tempField = document.querySelector('0');
+var requestLocation = Geolocation.getCurrentPosition(getLocation, errorOnGetLocation)
 
+getLocation = function(loc) {
+
+}
+
+errorOnGetLocation = function(error) { alert(error+": Couldn't get location") }
