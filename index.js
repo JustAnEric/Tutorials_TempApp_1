c5ffb94741d9ad02b160bdcfb21651c046e62eb6
WWEMGamer2 <73326807+WWEMGamer2@users.noreply.github.com>
2022-09-04 10:58:46 +1000

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var tempField = document.querySelector('0');
 var requestLocation = Geolocation.getCurrentPosition(getLocation, errorOnGetLocation)
 
 getLocation = function(loc) {
-
+    const crd = pos.coords;
+    print(crd)
 }
 
 errorOnGetLocation = function(error) { alert(error+": Couldn't get location") }
