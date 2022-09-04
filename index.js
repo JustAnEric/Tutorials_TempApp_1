555b1243c7babb38825d492e029b134e084084ab
WWEMGamer2 <73326807+WWEMGamer2@users.noreply.github.com>
2022-09-04 11:19:53 +1000

    added api

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 var prompt = document.querySelector('promt');
-var tempField = document.querySelector('0');
-var requestLocation = Geolocation.getCurrentPosition(getLocation, errorOnGetLocation)
+var tempField = document.querySelector('tempfield');
+
+const options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+};
 
 getLocation = function(loc) {
-    const crd = pos.coords;
-    print(crd)
+    const crd = loc.coords;
+    const longt = crd.longitude;
+    const latit = crd.latitude;
+    var resp = fetch(
+        `https://api.openweathermap.org/data/3.0/onecall?lat=${latit}&lon=${longt}&appid=ecd470505889fc70175bce7a20ecaa66`
+    )
+    resp.then(function(response) {
+        response.json['']
+    })
 }
 
 errorOnGetLocation = function(error) { alert(error+": Couldn't get location") }
+
+var requestLocation = navigator.geolocation.getCurrentPosition(getLocation, errorOnGetLocation)
